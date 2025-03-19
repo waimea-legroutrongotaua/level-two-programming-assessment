9897fe5f0d559e786d7d4345368c7904e1f2876c
waimea-legroutrongotaua <legroutrongotaua@waimea.school.nz>
2025-03-20 12:25:13 +1300

    idea added

--- src/Main.kt
+++ src/Main.kt
@@ -16,4 +16,17 @@
 fun main() {
     println("Hello World!")
 }
-
+//idea for game:
+// 1 base for each player (2 players)
+// 3 lanes with 3 differnt options for the player to put in each lane
+// Artillery > Riflemen, Fortress
+// Riflemen > Scouts
+// Scouts > Artillery
+//Special forces > Riflemen, Artillery and Scouts (only get one per game)
+// fortress > Riflemen and Scouts ( replaces scout when the other player is one round from beating you
+//  plyr 1 losing                       Tied                        plyr 2 losing
+//      (1)             (2)             (3)             (4)             (5)
+//   last stand                                                      last stand
+//   gain fort                                                        gain fort
+//
+//
