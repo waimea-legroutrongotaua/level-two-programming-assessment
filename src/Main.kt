21784a903ffbd147837a127526050de0d133fbef
waimea-legroutrongotaua <legroutrongotaua@waimea.school.nz>
2025-03-24 11:00:13 +1300

    more game ideas added

--- src/Main.kt
+++ src/Main.kt
@@ -14,19 +14,65 @@
 
 
 fun main() {
-    println("Hello World!")
 }
-//idea for game:
+//------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------
+// Game Name:
+//  
+//
+// idea for game:
+//
 // 1 base for each player (2 players)
-// 3 lanes with 3 differnt options for the player to put in each lane
+// 3 lanes with 3 different options for the player to put in each lane
 // Artillery > Riflemen, Fortress
 // Riflemen > Scouts
 // Scouts > Artillery
 //Special forces > Riflemen, Artillery and Scouts (only get one per game)
-// fortress > Riflemen and Scouts ( replaces scout when the other player is one round from beating you
+// fortress > Riflemen, special forces and Scouts ( replaces scout when the other player is one round from beating you
 //  plyr 1 losing                       Tied                        plyr 2 losing
 //      (1)             (2)             (3)             (4)             (5)
 //   last stand                                                      last stand
 //   gain fort                                                        gain fort
 //
+//if player 1 wins 1 battle and loses 2 at round (3) it will move to round (2) and if player 1 wins 3 battles and loses none then if
+// at round (2) moves it back to tied at round (3)
+// if player 1 loses at round (1) player 2 wins and the games over
+//if both players play the same options in the same lanes the or they both win one and tie on one they tie
+//and the round they were on restarts
+//  Scouts:
+//       ==========
+//            //
+//          //
+//       ==========
 //
+//  Riflemen:
+//       ==========
+//          \\//
+//          //\\
+//       ==========
+//
+//  Artillery:
+//       ==========
+//          //\\
+//          \\//
+//       ==========
+//
+//  Special Forces:
+//       ==========
+//          SPEC
+//          //\\
+//       ==========
+//
+//  Fortress:
+//       ==========
+//         ||--||
+//         ||  ||
+//       ==========
+//  lane:
+//       ==========     ==========      ==========
+//       |\     \1|     |\     \2|      |\     \3|
+//       |1\     \|     |2\     \|      |3\     \|
+//       ==========     ==========      ==========
+//------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------
+
